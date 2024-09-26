168b8eb415843a6ffc4510697378fd23cd7755ee
chrisfealy <chrismfealy@gmail.com>
2024-09-26 14:12:33 -0500

    updated site colors to match NeetCode

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,7 +7,7 @@ import { faFireFlameCurved, faSun } from "@fortawesome/free-solid-svg-icons"
 export default function Nav() {
   return (
     <>
-      <nav className="bg-gray-700 flex justify-between font-semibold">
+      <nav className="bg-nav-bg flex justify-between font-semibold">
         <div className="flex px-6 py-2 gap-4 items-center">
           <Link href="/" className="transition duration-300 ease-in-out hover:rotate-12"><Image src={logo} alt="NeetCode" width={36} /></Link>
           <ul className="flex gap-2">
@@ -19,14 +19,14 @@ export default function Nav() {
         </div>
 
         <div className="flex items-center gap-2 px-6">
-          <Link href="/pro" className="bg-emerald-900 px-4 py-1 flex items-center gap-2 rounded-xl hover:bg-emerald-950 transition duration-300 ease-in-out">
+          <Link href="/pro" className="bg-pro-btn-bg px-4 py-1 flex items-center gap-2 rounded-xl hover:bg-pro-btn-bg-hover transition duration-300 ease-in-out">
             <FontAwesomeIcon icon={faFireFlameCurved} />
             <div>Pro</div>
           </Link>
           <div className="nav-link hover:cursor-pointer ">
             <FontAwesomeIcon icon={faSun} />
           </div>
-          <div className="bg-blue-500 px-3 py-1 rounded-md hover:bg-blue-400 transition duration-300 ease-in-out hover:cursor-pointer">
+          <div className="bg-sign-in-btn px-3 py-1 rounded-md hover:bg-blue-400 transition duration-300 ease-in-out hover:cursor-pointer">
             Sign in
           </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,13 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "body-bg": "var(--body-background)",
+        "body-bg-2": "var(--body-background-2)",
+        "graph-bg": "var(--graph-background)",
+        "nav-bg": "var(--navbar-background)",
+        "pro-btn-bg": "var(--pro-btn-background)",
+        "pro-btn-bg-hover": "var(--pro-btn-background-hover)",
+        "sign-in-btn": "var(--sign-in-btn)",
       },
     },
   },
